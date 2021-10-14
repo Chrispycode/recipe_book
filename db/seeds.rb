b0893e8f199021b9a367c6116bbed3ff0d39a36a
chrispycode <christopherpauli@gmx.de>
2021-10-14 13:47:01 +0200

    Add Recipe model/controller and seeds 3. Rezept modellieren

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+images = Dir.new(Rails.root.to_s + "/app/assets/images/seed_images").select {|i| i.include? "jpg" }
+index = -1
+10.times do
+  index += 1
+  Recipe.create(title: Faker::Food.dish, author: Faker::Name.name, description: Faker::Food.description, image_url: "seed_images/#{images[index]}")
+end
---
+++ spec/factories/recipes.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :recipe do
+    title { "MyString" }
+    author { "MyString" }
+    description { "MyText" }
+    image_url { "MyString" }
+  end
+end
