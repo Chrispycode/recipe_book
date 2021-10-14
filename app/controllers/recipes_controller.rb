5a63f70c4703502a5cd26f02271c9ffd1e6534b8
chrispycode <christopherpauli@gmx.de>
2021-10-14 15:59:22 +0200

    Add form for creating recipes with ActiveStorage for uploading images 5. Neues Rezept hinzufügen

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,43 +14,33 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
 
-    respond_to do |format|
-      if @recipe.save
-        format.html { redirect_to @recipe, notice: "Recipe was successfully created." }
-        format.json { render :show, status: :created, location: @recipe }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
+    if @recipe.save
+      redirect_to @recipe, notice: "Recipe was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @recipe.update(recipe_params)
-        format.html { redirect_to @recipe, notice: "Recipe was successfully updated." }
-        format.json { render :show, status: :ok, location: @recipe }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
-      end
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: "Recipe was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @recipe.destroy
-    respond_to do |format|
-      format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to recipes_url, notice: "Recipe was successfully destroyed."
   end
 
   private
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
 
-    def recipe_params
-      params.require(:recipe).permit(:title, :author, :description, :image_url)
-    end
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :author, :description, :image_url, :uploaded_image)
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,18 @@
 class Recipe < ApplicationRecord
+  validates_presence_of :title, :description, :author
+  has_one_attached :uploaded_image
+  validate :presence_of_image
+
+  def image
+    return image_url if image_url.present?
+    Rails.application.routes.url_helpers.rails_blob_path(uploaded_image, only_path: true)
+  end
+
+  protected
+
+  def presence_of_image
+    return if uploaded_image.attached?
+    return if image_url.present?
+    errors.add(:image_url, "muss ausgefüllt werden")
+  end
 end
